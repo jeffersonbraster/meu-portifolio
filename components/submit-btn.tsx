b499f593ddcdd2e76bc6a02a22d50b41dc686387
Jefferson <jefferson.brandao@g.globo>
2023-10-13 12:17:23 -0300

    ts-ignore experimental

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+// ts-ignore because experimental_useFormStatus is not in the types
+// @ts-ignore
 import { experimental_useFormStatus as useFormStatus } from 'react-dom'
 
 import { FaPaperPlane } from 'react-icons/fa'
